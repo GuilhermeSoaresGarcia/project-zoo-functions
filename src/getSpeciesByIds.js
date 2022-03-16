ffc91540ae1478ed74044a992c139f4f512761a8
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-03-16 17:07:23 -0300

    Requisito 1, refatorado

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,12 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  if (!ids) return [];
-
-  const individual = 
-  data.species.filter((animal) => (animal.id === ids ? animal : []));
-  console.log(individual);
+function getSpeciesByIds(...ids) {
+  const { species } = data;
+  return species.filter((animals) => ids.some((receivedId) => animals.id === receivedId));
 }
-// getSpeciesByIds('baa6e93a-f295-44e7-8f70-2bcdc6f6948d');
 
 module.exports = getSpeciesByIds;
