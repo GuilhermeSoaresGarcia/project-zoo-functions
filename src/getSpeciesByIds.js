09cd62ea61da28d917c57ad8187370816221a959
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-03-16 15:49:59 -0300

    Requisito 1, retorno vazio

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(ids) {
-  // seu código aqui
+  if (!ids) return [];
+
+  const individual = 
+  data.species.filter((animal) => (animal.id === ids ? animal : []));
+  console.log(individual);
 }
+// getSpeciesByIds('baa6e93a-f295-44e7-8f70-2bcdc6f6948d');
 
 module.exports = getSpeciesByIds;
