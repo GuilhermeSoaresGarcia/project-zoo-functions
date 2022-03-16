e4365849251c5a6f0880bc821ae726a24ca170a4
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-03-16 17:40:22 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const { species } = data;
+  return species.filter((animalPopularName) => animalPopularName.name === animal)
+    .every((individual, index) => individual.residents[index].age > age);
 }
 
 module.exports = getAnimalsOlderThan;
