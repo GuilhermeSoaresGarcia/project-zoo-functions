443c7f163ec8361a912db008e6acaa9c8f8d390b
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-03-17 19:18:18 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,19 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const { employees } = data;
+  return employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const { employees } = data;
+  if (isManager(managerId)) {
+    const managedEmployees = employees.filter((person) => person.managers.includes(managerId));
+    const completeName = managedEmployees.map((names) =>
+      `${names.firstName} ${names.lastName}`);
+    return completeName;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
