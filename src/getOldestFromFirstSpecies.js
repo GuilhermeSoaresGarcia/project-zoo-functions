5a793bbedb409abbb70878dcde3083f03d5ff64f
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-03-16 22:21:05 -0300

    Requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const { employees } = data;
+  const { species } = data;
+  const animalThatIsResponsibleFor = employees.find((person) => person.id === id)
+    .responsibleFor[0];
+  return Object.values(species.find((subject) => subject.id === animalThatIsResponsibleFor)
+    .residents.sort((a, b) => b.age - a.age)[0]);
 }
 
 module.exports = getOldestFromFirstSpecies;
