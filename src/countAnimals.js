cb5adb73ed18ed644b74458af961faea3d14e4d9
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-03-17 23:02:31 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const { species } = data;
+  if (!animal) {
+    return species.reduce((acc, curr) => ({ ...acc, [curr.name]: curr.residents.length }), {});
+  }
+  if (Object.keys(animal).includes('sex')) {
+    return species.find((individual) => Object.values(animal)
+      .includes(individual.name)).residents.filter((gender) => (animal.sex === gender.sex)).length;
+  }
+  return species.find((individual) => Object.values(animal).includes(individual.name))
+    .residents.length;
 }
 
 module.exports = countAnimals;
