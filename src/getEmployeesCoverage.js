0ae454615e464329a0329e980f7e2cbe9924abd3
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-03-19 16:51:08 -0300

    Requisito 10, 3 primeiros testes

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function getEmployeesCoverage(info) {
+  const { species, employees } = data;
+  const employee = employees.find((person) => person.id === Object.values(info)[0]
+    || person.firstName === Object.values(info)[0]
+    || person.lastName === Object.values(info)[0]);
+  const animal = species.filter((individual) => employee.responsibleFor.includes(individual.id));
+
+  const result = {
+    id: `${employee.id}`,
+    fullName: `${employee.firstName} ${employee.lastName}`,
+    species: animal.map((teste) => teste.name),
+    locations: animal.map((teste) => teste.location),
+  };
+
+  return result;
 }
+
+console.log(getEmployeesCoverage({ name: 'Sharonda' }));
 
 module.exports = getEmployeesCoverage;
